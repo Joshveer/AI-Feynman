231dd305ad148ef3cfc3eac4784a4f545f46653e
Josh Grewal <veerjsg@outlook.com>
2024-07-05 08:10:40 -0400

    Ex6

--- examples/example.py
+++ examples/example.py
@@ -77,8 +77,8 @@ def test_noise_tolerance(pathdir, filename, noise_levels, BF_try_time=100, BF_op
 # Main execution
 if __name__ == "__main__":
     pathdir = "../example_data/"
-    filename = "WaterlooEx5.txt" # Add files here
-    piname = "buckinghamWaterlooEx5.txt"
+    filename = "WaterlooEx6.txt" # Add files here
+    piname = "buckinghamWaterlooEx6.txt"
     noise_levels = [0] # Add noise levels to test here
 
     test_noise_tolerance(pathdir, filename, noise_levels)
